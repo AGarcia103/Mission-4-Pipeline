d1dc8b457045f6d1be7be10711e3184890295c73
Agnes Garcia <agnesg@missionreadyhq.com>
2022-09-01 04:44:42 +1200

    removed navbar to see if test will pass

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
-import Navbar from "./HomeNavBar";
+//import Navbar from "./HomeNavBar";
 //import HomeContent from "./HomeContent";
 //import Footer from "./HomeFooter";
+import LoginModal from "./HomeLogin"
 
 
 export default function Home() {
   return (
     
       <div>
-        <Navbar />
+        <LoginModal/>
+       {/*<Navbar />*/} 
       {/*<HomeContent/>*/}
        {/* <Footer/>*/}
       </div>

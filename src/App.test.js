82473d1e9b4d020344e7f7bbc1aa655d36673335
Agnes Garcia <agnesg@missionreadyhq.com>
2022-09-01 19:07:25 +1200

    added snyk on config.yml file

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<Browser><App/></Browser>);
+  render(<App/>);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
